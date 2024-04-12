b335b20ab76d9efbc4a4bd68ea2ec4f880a9d51d
Ronni Rosario <ronnijosuerosario@gmail.com>
2024-04-11 21:44:04 -0400

    footer added

--- src/Components/Global/Footer.jsx
+++ src/Components/Global/Footer.jsx
@@ -1,13 +1,23 @@
-import "@Components/Home/About/About.css";
+import "./Footer.css";
+import ig from "@Images/Footer/ig.png"
 
 export default function Footer ()  {
-  return(
-   <section>
- <footer className="fercomtec-text text-white py-4 " >
-   
- </footer>
-   </section>
  
+  return(
+    <footer class="bg-footer footer-bold text-center text-lg-start text-white fixed-bottom ">
+    <section class="container py-4">
+        <div class="d-flex justify-content-center justify-content-lg-between align-items-center">
+            <div class="me-lg-5 mb-0 mb-lg-0">
+                <span>Copyright © 6to Desarrollo 2023-2024</span>
+            </div>
+            <div>
+                <a href="https://www.instagram.com/politecnicomariadelaaltagracia/" target="_blank" className="me-4 text-reset">
+                    <img src={ig} alt="Instagram" />
+                </a>
+            </div>
+        </div>
+    </section>
+</footer>
 
  
   )}
