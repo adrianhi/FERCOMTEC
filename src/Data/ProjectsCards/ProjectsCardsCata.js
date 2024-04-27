d96d88f71946bc22b41b4fa77b42d67d05c6a65f
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-04-27 10:33:50 -0400

    Mercadeo fixed

--- src/Data/ProjectsCards/ProjectsCardsCata.js
+++ src/Data/ProjectsCards/ProjectsCardsCata.js
@@ -218,7 +218,7 @@ export const projects = [
     image: stepUp,
   },
   {
-    group: "Comercio y mercadeo",
+    group: "Comercio y Mercadeo",
     name: "DAYFOODS",
     color: "bg-mercadeo",
     shortReview:
